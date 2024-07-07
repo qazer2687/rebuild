89d1b1b14b8f47bd5276242fb06160778656ddf2
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-07-07 21:44:11 +0100

    add support for nixos

--- rebuild.sh
+++ rebuild.sh
@@ -3,11 +3,10 @@
 username="qazer2687"
 reponame="dotfiles"
 
+mkdir -p /tmp/rebuild/ && cd /tmp/rebuild
+
 # darwin-rebuild
 if [ "$(uname)" == "Darwin" ]; then
-
-  # make both rebuild and log directory
-  mkdir -p /tmp/rebuild/ && cd /tmp/rebuild
 
   # build system configuration
   darwin-rebuild build --flake github:$username/$reponame#$(hostname) --refresh --option eval-cache false 2>/tmp/rebuild/log
@@ -16,18 +15,18 @@ if [ "$(uname)" == "Darwin" ]; then
   if [ $? -ne 0 ]; then
     cat /tmp/rebuild/log
   else
-    #nvd --color always diff /run/current-system /tmp/rebuild/result | grep --color=always -Ev '\[A\.\]|\[R\.\]'
-    nvd --color always diff /run/current-system /tmp/rebuild/result | awk '/\[A[.]\]|\[R[.]\]/{next} 1'
+    nvd diff /run/current-system /tmp/rebuild/result
   fi
 
 # nixos-rebuild
 elif [ "$(uname)" == "Linux" ]; then
-  olddrv=$(readlink -f /run/current-system)
-  sudo nixos-rebuild switch --flake github:qazer2687/dotfiles#$(hostname) --refresh --option eval-cache false >/dev/null 2>"${ERROR_LOG}"
+  # build system configuration
+  sudo nixos-rebuild build --flake github:$username/$reponame#$(hostname) --refresh --option eval-cache false 2>/tmp/rebuild/log
+
+  # supress output unless an error occurs
   if [ $? -ne 0 ]; then
-    cat "$ERROR_LOG"
+    cat /tmp/rebuild/log
   else
-    newdrv=$(readlink -f /run/current-system)
-    nvd diff "$olddrv" "/run/current-system"
+    nvd diff /run/current-system /tmp/rebuild/result
   fi
 fi
